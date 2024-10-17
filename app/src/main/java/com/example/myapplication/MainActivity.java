e0a3b5c3e6de29ea6f9dd5ae2266f316673e056e
mklzc <mklzc102@qq.com>
2024-10-17 09:18:39 +0800

    初步完善了程序框架

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,10 +6,14 @@ import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,75 +25,75 @@ import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<String> mData = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView tv = new TextView(this);
-        String tmp = getSmsInPhone();
-        tv.setText(tmp);
-        ScrollView sv = new ScrollView(this);
-        sv.addView(tv);
-        setContentView(sv);
-    }
 
-    public String getSmsInPhone() {
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        getSmsInPhone();
+        MyAdapter mAdapter = new MyAdapter(mData);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+    private boolean judge(String strBody) {
+        final List<String> keywords = Arrays.asList("驿收发", "快递");
+        for (String kw : keywords) {
+            if (strBody.contains(kw)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void getSmsInPhone() {
         final String SMS_URI_ALL = "content://sms/";
-        StringBuilder smsBuilder = new StringBuilder();
+
         // StringBuilder 与 String 不同的是 StringBuilder 能够被多次修改，而不产生新的使用对象
         try {
             Uri uri = Uri.parse(SMS_URI_ALL);
-
+            // parse 将字符串转化为 uri 对象
             String[] projection = new String[] { "_id", "address", "person",
                     "body", "date", };
             Cursor cur = getContentResolver().query(uri, projection, null,
                     null, "date desc");
             assert cur != null;
-            if (cur.moveToFirst()) {
-                int index_Address = cur.getColumnIndex("address");
-                int index_Person = cur.getColumnIndex("person");
-                int index_Body = cur.getColumnIndex("body");
-                int index_Date = cur.getColumnIndex("date");
-
-                do {
-                    String strAddress = cur.getString(index_Address);
-                    int intPerson = cur.getInt(index_Person);
-                    String interbody = cur.getString(index_Body);
-                    long longDate = cur.getLong(index_Date);
-
-                    @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat(
-                            "yyyy-MM-dd hh:mm:ss");
-                    Date d = new Date(longDate);
-                    String strDate = dateFormat.format(d);
-
-                    if (Objects.equals(strAddress, "10687071002656890147")) {
-                        smsBuilder.append("[ ");
-                        smsBuilder.append(strAddress).append(", ");
-                        smsBuilder.append(intPerson).append(", ");
-                        smsBuilder.append(interbody).append(", ");
-                        smsBuilder.append(strDate).append(", ");
-                        smsBuilder.append(" ]\n\n");
-                    }
-                } while (cur.moveToNext());
-
-                if (!cur.isClosed()) {
-                    cur.close();
-                    cur = null;
+            int index_Address = cur.getColumnIndex("address");
+            int index_Person = cur.getColumnIndex("person");
+            int index_Body = cur.getColumnIndex("body");
+            int index_Date = cur.getColumnIndex("date");
+            while (cur.moveToNext()) {
+                StringBuilder smsBuilder = new StringBuilder();
+                String strAddress = cur.getString(index_Address);
+                int intPerson = cur.getInt(index_Person);
+                String strBody = cur.getString(index_Body);
+                long longDate = cur.getLong(index_Date);
+                @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                Date d = new Date(longDate);
+                String strDate = dateFormat.format(d);
+                if (judge(strBody)) {
+                    smsBuilder.append("[ ");
+                    smsBuilder.append(strAddress).append(", ");
+                    smsBuilder.append(intPerson).append(", ");
+                    smsBuilder.append(strBody).append(", ");
+                    smsBuilder.append(strDate).append(", ");
+                    smsBuilder.append(" ]");
                 }
-            } else {
-                smsBuilder.append("no result!");
+                if (smsBuilder.length() != 0) {
+                    mData.add(smsBuilder.toString());
+                }
             }
-            smsBuilder.append("getSmsInPhone has executed!");
+            if (!cur.isClosed()) {
+                cur.close();
+            }
         } catch (SQLiteException ex) {
             Log.d("SQLiteException in getSmsInPhone", Objects.requireNonNull(ex.getMessage()));
         }
-        return smsBuilder.toString();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    final private List<String> mData;
+
+    public MyAdapter(List<String> data) {
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = mData.get(position);
+        holder.textView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
